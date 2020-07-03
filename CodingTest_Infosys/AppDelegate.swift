88d79f8deddb1baa8cecf4c32d377cd8d9ad2adb
Niranjan <vaddepalli.niranjan@gmail.com>
2020-07-03 16:09:53 +0530

    Removed Storyboard file. and loading the view programatically

--- CodingTest_Infosys/AppDelegate.swift
+++ CodingTest_Infosys/AppDelegate.swift
@@ -11,10 +11,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if #available(iOS 13.0, *) { } else {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            let mainViewController = ViewController()
+            let mainNavigationController = UINavigationController(rootViewController: mainViewController)
+            self.window!.rootViewController = mainNavigationController
+            self.window!.makeKeyAndVisible()
+        }
         return true
     }
 
