82d366c930e284f65dd5d1444c60d6572b938a82
Niranjan <vaddepalli.niranjan@gmail.com>
2020-07-03 16:03:46 +0530

    FIRST COMMIT: Repository Created for the Coding Test.

---
+++ CodingTest_Infosys/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingTest_Infosys
+//
+//  Created by Niranjan on 03/07/20.
+//  Copyright © 2020 Niranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
